6463a5d75e96974fc9186b215ef06c62da4e0c27
AquanJSW <zhdlcc@gmail.com>
2021-11-11 14:39:41 +0800

    update lv6's first step.

--- 6_zip.py
+++ 6_zip.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
-import zipfile
+from zipfile import ZipFile
+import codecs
 
 FILE_PATH = './channel.zip'
 
 def get_fname(n: int):
     return str(n) + '.txt'
 
-def insight_file(fname: str):
-    try:
-        with open(fname) as fp:
-            string = fp.readline().split()[-1].removesuffix('\n')
-    except:
-        print("FileNotFoundError: {}".format(fname))
+def insight_file(zf: ZipFile, fname: str):
+    if fname not in zf.namelist():
+        print("FileNotExist: {}".format(fname))
         return False
+    
+    content = list(map(codecs.decode, zf.open(fname).readlines()))
+    string = content[0].strip().split()[-1].removesuffix('\n')
 
     try:
         next_number = eval(string)
         return next_number
     except:
-        with open(fname) as fp:
-            print(fname, fp.readlines())
+        print('{}\n{}'.format(fname, ''.join(content)))
         return False
 
 if __name__ == '__main__':
-    os.chdir(DIR)
-    files = os.listdir()
-    max_iter = len(files) - 1
+    zf = ZipFile(FILE_PATH)
+    
+    max_iter = len(zf.namelist()) - 1
+
     fname = '90052.txt'
+
     for _ in range(max_iter):
-        ans = insight_file(fname)
+        ans = insight_file(zf, fname)
         if ans:
             fname = get_fname(ans)
         else:
