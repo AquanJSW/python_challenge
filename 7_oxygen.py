c858ebbb85cce0a2bd977a77a60c42f6a8104619
AquanJSW <zhdlcc@gmail.com>
2021-11-18 20:24:00 +0800

    udpate lv7

--- 7_oxygen.py
+++ 7_oxygen.py
@@ -2,10 +2,13 @@
 
 from urllib import request
 from PIL import Image
+import codecs
 import io
 
 PNG_URL = 'http://www.pythonchallenge.com/pc/def/oxygen.png'
 
 if __name__ == '__main__':
     r = request.urlopen(PNG_URL)
-    img = Image.open(io.BytesIO()
+    data = r.read()
+    img = Image.open(io.BytesIO(data))
+    pass
